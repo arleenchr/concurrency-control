ef0f2114c335c6aade778109a1bba93b37dd1033
arleenchr <13521059@mahasiswa.itb.ac.id>
2023-11-30 09:38:38 +0700

    Feat: Schedule read from input & instruction getter from transaction id

--- src/schedule.py
+++ src/schedule.py
@@ -6,12 +6,37 @@ class Instruction:
         self.item = item # data item, e.g: item A in R(A)
         self.type = type # read, write, commit, abort
 
+    def __init__(self, input_sequence):
+        # input_sequence: Rx(A) / Wx(A) / Cx
+        if (input_sequence[0]=='R'):
+            self.type = 'read'
+            self.transaction_id = input_sequence[1]
+            self.item = input_sequence[3]
+        elif (input_sequence[0]=='W'):
+            self.type = 'write'
+            self.transaction_id = input_sequence[1]
+            self.item = input_sequence[3]
+        elif (input_sequence[0]=='C'):
+            self.type = 'commit'
+            self.transaction_id = input_sequence[1]
+            self.item = ''
+
 class Schedule:
     def __init__ (self):
         self.instructions = Queue() # list (queue) of instructions
         self.rollbacked_list = [] # list of transactions rollbacked
         self.logging = Queue() # list (queue) of final schedule
         
+    def __init__ (self, input_sequence):
+        self.instructions = Queue() # list (queue) of instructions
+        self.rollbacked_list = [] # list of transactions rollbacked
+        self.logging = Queue() # list (queue) of final schedule
+        # input_sequence: Rx(A);Wx(A);Cx;Rx(B);Cx
+        input_list = input_sequence.split(";")
+        for input in input_list:
+            input_instruction = Instruction(input)
+            self.instructions.put(input_instruction)
+
     def add_instruction(self, instruction):
         self.instructions.put(instruction)
 
@@ -28,4 +53,9 @@ class Schedule:
         self.logging.put(instruction)
         return instruction
     
-    
+    def get_logged_instructions(self, transaction_id):
+        logged = [] 
+        for instruction in list(self.logging.queue):
+            if (instruction.transaction_id == transaction_id):
+                logged.append(instruction)
+        return logged
